dd2864a9c7ee397db44f06eae38da4c212996109
Phillip Campbell <spyyderz@gmail.com>
2010-07-05 17:55:25 -0400

    frustrating getting over some of the lack of documentatino for express. split haml into layout.

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 var sys = require("sys");
-var readFile = require("fs").readFile;
-
 require('express');
 
+var music_control = require("./lib/music-control"),
+    play_list     = require("./lib/playlist");
+
 configure(function(){
+  use(Logger)
+  use(Static)
   set('root', __dirname)
 })
 
+
+
 get('/', function(){
   this.render('index.html.haml', {
+
+  });
+});
+
+get('/snapshot', function(){
+  this.render('index.html.haml', {
     layout: false
-  })
-})
+  });
+});
+
+get('/updates', function(){
+  this.render('index.html.haml', {
+    layout: false
+  });
+});
+
+
 
 run();
